8888c8a873f1d2c59111ced991ae970313e256d1
CemPesdereli <cempesdereli@gmail.com>
2024-06-28 03:31:49 +0300

    first commit

---
+++ src/main/java/com/cem/datajpabasic/DatajpabasicApplication.java
@@ -0,0 +1,13 @@
+package com.cem.datajpabasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatajpabasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatajpabasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cem/datajpabasic/DatajpabasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cem.datajpabasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatajpabasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
